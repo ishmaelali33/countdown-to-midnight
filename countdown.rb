5f10d1ff246c4eeb2dff6e635ba9bb70e97103d8
ishmaelali33 <ishmaelali33@gmail.com>
2020-10-25 11:45:43 -0400

    commit tree b1c17c6236f29139afd2eb5a591d162188e74055

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
-end
+countdown_with_sleep = 0
+def countdown_with_sleep
+while countdown_with_sleep < 11
+     puts "#countdown_with_sleep{10} to bedtime "
+    countdown_with_sleep -= 1
+    end
+  end
+ countdown_with_sleep{10.seconds} 
